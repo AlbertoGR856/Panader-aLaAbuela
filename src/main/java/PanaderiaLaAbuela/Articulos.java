5b0a19489da053456fd467643402565a01e9b3a2
Alberto <albertogr578@gmail.com>
2020-06-02 12:00:01 +0200

    Correción articulos y bolsas de productos

--- src/main/java/PanaderiaLaAbuela/Articulos.java
+++ src/main/java/PanaderiaLaAbuela/Articulos.java
@@ -13,17 +13,20 @@ public enum Articulos {
 
     BOTELLA_MONSTER(1, "botella de monster ", 4),
     BOLSA_PATATAS_FRITAS(2, "bolsa de patatas fritas", 2),
-    PIZZA_PERICOLOSA(3, "pack de latas de coca-cola ", 3),
+    PACK_LATAS_COCA_COLA(3, "pack de latas de coca-cola ", 3),
     BOLSA_PAPA_DELTA(4, "bolsa de papa-delta ", 1),
     GUSANITOS_RISI(5, "gusanitos RISI", 1.5),
     PAQUETE_MEDIANOCHES(6, "paquete de medianoches ", 1.5),
     PAQUETE_DONUTS(7, "paquete de donuts ", 1.5),
     BOLSA_PALOMITAS_KETCHUP(8, "bolsa de palomitas sabor ketchup ", 1.5),
     JUDIAS_LATA_MERCA_POWER(9, "judías de lata merca-power ", 1.5),
-    BOLSITA_CHUCHERIAS(10, "bolsita de chucherías", 1.5),
-    RED_BULL(10, "red bull", 2),
-    COCA_COLA(11, "coca-cola", 1.7),
-    BARRAS_PAN(12, "2 barras de pan", 1);
+    AZUCAR(10, "azúcar", 1.5),
+    BOLSITA_CHUCHERIAS(11, "bolsita de chucherías", 1.5),
+    RED_BULL(12, "red bull", 2),
+    COCA_COLA(13, "coca-cola", 1.7),
+    BARRAS_PAN(14, "2 barras de pan", 1),
+    BOTELLA_OXIGENO_ACTIVO(15, "botella de oxígeno activo", 2),
+    LAYS_CAMPESINAS(16, "lays sabor campesinas", 2);
 
     private int referenciaArticulo;
     private String nombreArticulo;
