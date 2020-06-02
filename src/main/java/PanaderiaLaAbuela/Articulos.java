4378b5d5f3c8ef45db31258d4d5f79b3ad5b64ba
Alberto <albertogr578@gmail.com>
2020-06-02 11:29:56 +0200

    Añade constructor por defecto, getter y setters y toString a la clase Articulos

--- src/main/java/PanaderiaLaAbuela/Articulos.java
+++ src/main/java/PanaderiaLaAbuela/Articulos.java
@@ -10,19 +10,46 @@ package PanaderiaLaAbuela;
  * @author GR6
  */
 public enum Articulos {
-    
+
     BOTELLA_MONSTER(1, "botella de monster ", 4),
-    BOLSA_PATATAS_FRITAS(2, "bolsa de patatas fritas",2),
+    BOLSA_PATATAS_FRITAS(2, "bolsa de patatas fritas", 2),
     PIZZA_PERICOLOSA(3, "pack de latas de coca-cola ", 3),
-    BOLSA_PAPA_DELTA(4,"bolsa de papa-delta ",1),
+    BOLSA_PAPA_DELTA(4, "bolsa de papa-delta ", 1),
     GUSANITOS_RISI(5, "gusanitos RISI", 1.5),
     PAQUETE_MEDIANOCHES(6, "paquete de medianoches ", 1.5),
-    PAQUETE_DONUTS7, "paquete de donuts ", 1.5),
+    PAQUETE_DONUTS(7, "paquete de donuts ", 1.5),
     BOLSA_PALOMITAS_KETCHUP(8, "bolsa de palomitas sabor ketchup ", 1.5),
-    JUDIAS_LATA_MERCA_POWER(9,"judías de lata merca-power ", 1.5),
+    JUDIAS_LATA_MERCA_POWER(9, "judías de lata merca-power ", 1.5),
     BOLSITA_CHUCHERIAS(10, "bolsita de chucherías", 1.5),
     RED_BULL(10, "red bull", 2),
     COCA_COLA(11, "coca-cola", 1.7),
     BARRAS_PAN(12, "2 barras de pan", 1);
-    
+
+    private int referenciaArticulo;
+    private String nombreArticulo;
+    private double precioArticulo;
+
+    private Articulos(int referenciaArticulo, String nombreArticulo, double precioArticulo) {
+        this.referenciaArticulo = referenciaArticulo;
+        this.nombreArticulo = nombreArticulo;
+        this.precioArticulo = precioArticulo;
+    }
+
+    public int getReferenciaArticulo() {
+        return referenciaArticulo;
+    }
+
+    public String getNombreArticulo() {
+        return nombreArticulo;
+    }
+
+    public double getPrecioArticulo() {
+        return precioArticulo;
+    }
+
+    @Override
+    public String toString() {
+        return this.getNombreArticulo() + " (que cuesta " + this.getPrecioArticulo() + "€)\n";
+    }
+
 }
