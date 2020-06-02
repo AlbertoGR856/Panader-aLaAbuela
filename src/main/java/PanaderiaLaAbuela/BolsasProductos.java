9a099c9827fe5cd585b0aec9fe25c1b45b1909be
Alberto <albertogr578@gmail.com>
2020-06-02 11:36:17 +0200

    Añade constructor por defecto, getters y setters y toString a la clase BolsaProductos

--- src/main/java/PanaderiaLaAbuela/BolsasProductos.java
+++ src/main/java/PanaderiaLaAbuela/BolsasProductos.java
@@ -11,6 +11,36 @@ package PanaderiaLaAbuela;
  */
 public enum BolsasProductos {
     
+    BOLSA_1(1, "botella de monster "),
+    BOLSA_2(2, "bolsa de patatas fritas"),
+    BOLSA_3(3, "pack de latas de coca-cola "),
+    BOLSA_4(4, "bolsa de papa-delta "),
+    BOLSA_5(5, "gusanitos RISI");
+    
+    private int referenciaBolsaProductos;
+    private String nombreBolsaProductos;
+
+    private BolsasProductos(int referenciaBolsaProductos, String nombreBolsaProductos) {
+        this.referenciaBolsaProductos = referenciaBolsaProductos;
+        this.nombreBolsaProductos = nombreBolsaProductos;
+    }
+
+    public int getReferenciaBolsaProductos() {
+        return referenciaBolsaProductos;
+    }
+
+    public void setReferenciaBolsaProductos(int referenciaBolsaProductos) {
+        this.referenciaBolsaProductos = referenciaBolsaProductos;
+    }
+
+    public String getNombreBolsaProductos() {
+        return nombreBolsaProductos;
+    }
+
+    public void setNombreBolsaProductos(String nombreBolsaProductos) {
+        this.nombreBolsaProductos = nombreBolsaProductos;
+    }
+    
     
     
 }
