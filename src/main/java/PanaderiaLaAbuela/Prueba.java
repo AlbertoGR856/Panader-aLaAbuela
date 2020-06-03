368380eedd20a504f1cf0fbc836cd02b0ada33c0
Alberto <albertogr578@gmail.com>
2020-06-03 09:36:51 +0200

    Crea la clase Prueba con un main

--- src/main/java/PanaderiaLaAbuela/Prueba.java
+++ src/main/java/PanaderiaLaAbuela/Prueba.java
@@ -5,10 +5,22 @@
  */
 package PanaderiaLaAbuela;
 
+import java.util.Scanner;
+
 /**
  *
  * @author GR6
  */
 public class Prueba {
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        boolean addElement = true;
+        int sel; 
+        System.out.println("--- SELECCIONE SU PEDIDO ---");
+        System.out.println("Las bolsas de prodcutos a excepcion de la numero 5 "
+                + "tienen un descuento de 1,5€");
+       
+    }
     
 }
