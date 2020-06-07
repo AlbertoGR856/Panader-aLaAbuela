77701e7007818f3aac04b8c8d6709f3bcfcdadec
Alberto <albertogr578@gmail.com>
2020-06-07 12:07:03 +0200

    Añade comentarios a la clase Pedido

--- src/main/java/PanaderiaLaAbuela/Pedido.java
+++ src/main/java/PanaderiaLaAbuela/Pedido.java
@@ -14,11 +14,16 @@ import java.util.Random;
  */
 public class Pedido {
 
+    // Declaramos los atributos de la clase Pedido, creando dos ArrayList de 
+    //cada enum
+    
     private String referencia;
     private ArrayList<Articulos> articulosPedido;
     private ArrayList<BolsasProductos> bolsaProductosPedido;
     private double total;
 
+    // Creamos un metodo publico para la clse pedido donde se creara un pedido
+    // de forma aleatoria.
     public Pedido() {
         Random r = new Random();
         this.referencia = Integer.toString(r.nextInt(10000000));
@@ -26,27 +31,31 @@ public class Pedido {
         this.bolsaProductosPedido = new ArrayList<>();
     }
 
+    // Creamos un método que devuelva un articulo
     public void nuevoArticulo(Articulos a) {
         this.articulosPedido.add(a);
         this.calcularPrecio(a);
     }
     
-    
+    // Creamos un método que devuelva una BolsaProducto
      public void nuevoBolsaProducto(BolsasProductos b) {
         this.bolsaProductosPedido.add(b);
         this.calcularPrecio(a);
     }
 
 
+     // Crearemos un metodo que devuelva el calculo del precio del articulo
     private void calcularPrecio(Articulos a) {
         this.total += a.getPrecioArticulo();
     }
 
+    // Crearemos un metodo que devuelva el calculo del precio de una bolsa de producto
     private void restarPrecio(BolsasProductos a) {
         this.total -= 1.5;
     }
 
-   
+    
+   //Getters y Setters
     public String getReferencia() {
         return referencia;
     }
@@ -71,6 +80,8 @@ public class Pedido {
         this.total = subtotal;
     }
 
+    
+    // Creamos un metodo toString con un for para determinar que contiene el articulo
     @Override
     public String toString() {
          String contenidoArticulo = "";
